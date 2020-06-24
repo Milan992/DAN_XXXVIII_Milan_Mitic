94fe310d6c28bed530a32fda93d778ac5c81213d
Milan992 <mitic.gsm@gmail.com>
2020-06-24 15:02:11 +0200

    initial commit

---
+++ DAN_XXXVIII_Milan_Mitic/TruckApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static EventWaitHandle waitHandle = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(() => Go());
+            Thread t2 = new Thread(() => Go2());
+            t.Start();
+            t2.Start();
+
+            Console.ReadLine();
+        }
+
+        public static void Go()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("   " + i);
+            }
+            waitHandle.Set();
+        }
+
+        public static void Go2()
+        {
+            waitHandle.WaitOne();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("aaa " + i);
+            }
+        }
+    }
+}
